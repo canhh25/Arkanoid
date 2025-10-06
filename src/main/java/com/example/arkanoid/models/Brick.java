3bc43bb5af9362dc6b493276e7754dddf41d4fc6
Nguyễn Hữu Cảnh <huucanh2556@gmail.com>
2025-10-06 23:03:01 +0700

    Ball, Brck, MoveableObject and GameObject, movement and collision

--- src/main/java/com/example/arkanoid/models/Brick.java
+++ src/main/java/com/example/arkanoid/models/Brick.java
@@ -1,14 +1,25 @@
 package com.example.arkanoid.models;
 
-public class Brick extends MovableObject {
+public class Brick extends GameObject {
     private int hitPoints;
     private String type;
 
-    public void takeHit(){}
+    public Brick(double x, double y, double width, double height, int hitPoints, String type) {
+        super(x, y, width, height);
+        this.hitPoints = hitPoints;
+        this.type = type;
+    }
+
+    public void takeHit() {
+        hitPoints--;
+    }
 
     public boolean isDestroyed() {
         return this.hitPoints <= 0;
     }
 
+    public String getType() {
+        return type;
+    }
 
 }
