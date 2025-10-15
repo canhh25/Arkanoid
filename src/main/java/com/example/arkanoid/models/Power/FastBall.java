73815f0abf34b4497fd11be89b4db43dd989cf27
grace21dd <ngoisao00811@gmail.com>
2025-10-15 10:51:01 +0700

    feat: Add FastBall.java

---
+++ src/main/java/com/example/arkanoid/models/Power/FastBall.java
@@ -0,0 +1,33 @@
+package com.example.arkanoid.models.Power;
+
+import com.example.arkanoid.models.Ball;
+
+public class FastBall extends PowerUp {
+    private static final double SPEED_SCALE = 1.5;
+    private double baseDx;
+    private double baseDy;
+
+    public FastBall(double x, double y) {
+        super(x, y, 14, 14, "FastBall", 10000, "/images/ball/ball.png");
+    }
+
+    @Override
+    public void applyEffect(Ball ball) {
+        if (!isActive) {
+            baseDx = ball.getDx();
+            baseDy = ball.getDy();
+            ball.setDx(baseDx * SPEED_SCALE);
+            ball.setDy(baseDy * SPEED_SCALE);
+            isActive = true;
+        }
+    }
+
+    @Override
+    public void removeEffect(Ball ball) {
+        if (isActive) {
+            ball.setDx(baseDx);
+            ball.setDy(baseDy);
+            isActive = false;
+        }
+    }
+}
