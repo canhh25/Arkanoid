7ba2e1d2dd9659fb826737615523b30a050279ec
xuantung159 <dxtung159@gmail.com>
2025-10-18 08:44:32 +0700

    Add PowerUp Animation

--- src/main/java/com/example/arkanoid/models/Power/PowerUpManager.java
+++ src/main/java/com/example/arkanoid/models/Power/PowerUpManager.java
@@ -6,26 +6,30 @@ import java.util.List;
 import javafx.scene.canvas.GraphicsContext;
 
 public class PowerUpManager {
-    private static List<PowerUp> activePowerUps = new ArrayList<>();
+//    private static List<PowerUp> activePowerUps = new ArrayList<>();
     private static GameManager gameManager;
-
+    private static PowerUpType typePower;
     public static void setGameManager(GameManager gm) {
         gameManager = gm;
     }
 
-    public static void addPowerUp(PowerUp powerUp) {
-        activePowerUps.add(powerUp);
-    }
+//    public static void addPowerUp(PowerUp powerUp) {
+//        activePowerUps.add(powerUp);
+//    }
 
-    public static void updatePowerUps(Paddle paddle) {
+    public static void updatePowerUps(Paddle paddle, Ball ball) {
         if (gameManager == null || paddle == null) return;
 
         for (int i = activePowerUps.size() - 1; i >= 0; i--) {
             PowerUp powerUp = activePowerUps.get(i);
-            powerUp.update(); // Cập nhật cả vị trí và animation
 
+            if(powerUp instanceof ExtraLifePowerUp) {
+                ((ExtraLifePowerUp) powerUp).update();
+            }
+
+            // Power chạm vào paddle
             if (powerUp.intersects(paddle)) {
-                applyPowerUpEffect(powerUp, paddle);
+                applyPowerUpEffect(powerUp, paddle, ball);
                 activePowerUps.remove(i);
                 continue;
             }
@@ -36,20 +40,20 @@ public class PowerUpManager {
         }
     }
 
-    private static void applyPowerUpEffect(PowerUp powerUp, Paddle paddle) {
-        powerUp.applyEffect(paddle);
-
-        if (powerUp.getType().equals("EXTRA_LIFE")) {
+    private static void applyPowerUpEffect(PowerUp powerUp, Paddle paddle, Ball ball) {
+        powerUp.applyEffect(ball);
+        typePower = PowerUpType.EXTRA_LIFE;
+        if (typePower.equals(PowerUpType.EXTRA_LIFE)) {
             gameManager.lives++;
             System.out.println("Nhận powerup: Thêm mạng! Số mạng: " + gameManager.lives);
         }
     }
 
-    public static void renderPowerUps(GraphicsContext gc) {
-        for (PowerUp powerUp : activePowerUps) {
-            powerUp.render(gc);
-        }
-    }
+//    public static void renderPowerUps(GraphicsContext gc) {
+//        for (PowerUp powerUp : activePowerUps) {
+//            powerUp.render(gc);
+//        }
+//    }
 
     public static void clearPowerUps() {
         activePowerUps.clear();
