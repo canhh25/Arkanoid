a0714e5f7c066b4ea5e86f657c0ee8c2c7b98c23
grace21dd <ngoisao00811@gmail.com>
2025-10-19 23:19:14 +0700

    Update Ball.java, Paddle.java from master

--- src/main/java/com/example/arkanoid/models/Ball.java
+++ src/main/java/com/example/arkanoid/models/Ball.java
@@ -1,17 +1,33 @@
 package com.example.arkanoid.models;
 
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class Ball extends MovableObject {
     public double dx, dy;
-    public static final double WIDTH = 14;
-    public static final double HEIGHT = 14;
+    public static final double BALL_WIDTH = 14;
+    public static final double BALL_HEIGHT = 14;
 
     public Ball(double x, double y) {
-        super(x, y, WIDTH, HEIGHT, "/images/ball/ball.png");
+        super(x, y, BALL_WIDTH, BALL_HEIGHT, "/images/ball/ball.png");
 
         // Vận tốc ban đầu
         dx = 2;
         dy = -2;
+    }
+
+    @Override
+    public void update() {
+        x += dx;
+        y += dy;
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        // Vẽ brick bằng ảnh từ GameObject - giữ nguyên chức năng hiện có
+        if (getImage() != null) {
+            gc.drawImage(getImage(), getX(), getY(), getWidth(), getHeight());
+        }
     }
 
     public double getDx() {
@@ -28,11 +44,5 @@ public class Ball extends MovableObject {
 
     public void setDy(double dy) {
         this.dy = dy;
-    }
-
-    @Override
-    public void update() {
-        x += dx;
-        y += dy;
     }
 }
