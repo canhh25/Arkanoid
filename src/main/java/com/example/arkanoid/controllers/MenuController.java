9b610d3bec886e497144b8f42b455c53a1708c7c
grace21dd <ngoisao00811@gmail.com>
2025-10-20 10:13:57 +0700

    Update MenuController

--- src/main/java/com/example/arkanoid/controllers/MenuController.java
+++ src/main/java/com/example/arkanoid/controllers/MenuController.java
@@ -1,4 +1,47 @@
 package com.example.arkanoid.controllers;
 
+import com.example.arkanoid.main.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class MenuController {
+
+    @FXML
+    private Button btnStart, btnHelp, btnEsc, btnSound, btnBack;
+
+    @FXML
+    private void handleStart(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleHelp(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/arkanoid/views/ViewMenu.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = new Stage();
+        stage.setTitle("Instructions");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+
+    @FXML
+    private void handleEsc(ActionEvent event) {
+        Stage stage = (Stage) btnExit.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void handleSound(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleBack(ActionEvent event) {
+
+    }
+
 }
