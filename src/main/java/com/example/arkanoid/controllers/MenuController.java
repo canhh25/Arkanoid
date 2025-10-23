4acdc433a60e02ea83c6b3e69609174857260f9d
grace21dd <ngoisao00811@gmail.com>
2025-10-23 22:05:28 +0700

    fix MenuController and Main
    # Conflicts:
    #	src/main/java/com/example/arkanoid/controllers/MenuController.java
    #	src/main/java/com/example/arkanoid/main/Main.java

--- src/main/java/com/example/arkanoid/controllers/MenuController.java
+++ src/main/java/com/example/arkanoid/controllers/MenuController.java
@@ -4,26 +4,53 @@ import com.example.arkanoid.main.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class MenuController {
+    private static final int WIDTH = 960;
+    private static final int HEIGHT = 640;
 
+    public void openGame(Stage stage) {
+        try {
+
+            Canvas canvas = new Canvas(WIDTH, HEIGHT);
+            GraphicsContext gc = canvas.getGraphicsContext2D();
+
+            StackPane root = new StackPane(canvas);
+            Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+            GameController gameController = new GameController(gc);
+
+            stage.setTitle("Arkanoid");
+            stage.setScene(scene);
+            stage.show();
+
+            gameController.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @FXML
     private Button btnStart, btnHelp, btnEsc, btnSound, btnBack;
 
     @FXML
     private void handleStart(ActionEvent event) {
-
+        Stage stage = (Stage) btnStart.getScene().getWindow();
+        openGame(stage);
     }
 
+
     @FXML
-    private void handleHelp(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/arkanoid/views/MenuView.fxml"));
+    private void handleHelp(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/arkanoid/views/HelpView.fxml"));
         Parent root = loader.load();
 
         Stage stage = new Stage();
@@ -46,7 +73,8 @@ public class MenuController {
 
     @FXML
     private void handleBack(ActionEvent event) {
-
+        Stage stage = (Stage) btnHelp.getScene().getWindow();
+        openGame(stage);
     }
 
 }
