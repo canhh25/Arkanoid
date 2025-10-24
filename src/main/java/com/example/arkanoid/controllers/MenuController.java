f579cf6e377caedb75390d4607331d427ab6c723
grace21dd <ngoisao00811@gmail.com>
2025-10-25 00:22:02 +0700

    fix volume in MenuController and refactor Main

--- src/main/java/com/example/arkanoid/controllers/MenuController.java
+++ src/main/java/com/example/arkanoid/controllers/MenuController.java
@@ -12,15 +12,28 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+import javafx.geometry.Rectangle2D;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
 
 public class MenuController {
     private static final int WIDTH = 960;
     private static final int HEIGHT = 640;
 
+    @FXML
+    private Button btnStart, btnHelp, btnEsc, btnBack;
+
+    @FXML
+    private ImageView btnVolume;
+
+    private boolean mute = false;
+    private Image volumeSprite;
+
+
     public void openGame(Stage stage) {
         try {
-
             Canvas canvas = new Canvas(WIDTH, HEIGHT);
             GraphicsContext gc = canvas.getGraphicsContext2D();
 
@@ -39,25 +52,24 @@ public class MenuController {
         }
     }
     @FXML
-    private Button btnStart, btnHelp, btnEsc, btnSound, btnBack;
-
-    @FXML
     private void handleStart(ActionEvent event) {
         Stage stage = (Stage) btnStart.getScene().getWindow();
         openGame(stage);
     }
 
-
     @FXML
     private void handleHelp(ActionEvent event) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/arkanoid/views/HelpView.fxml"));
-        Parent root = loader.load();
-
-        Stage stage = new Stage();
-        stage.setTitle("Instructions");
-        stage.setScene(new Scene(root));
-        stage.show();
-
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/arkanoid/views/HelpView.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Instructions");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -67,14 +79,28 @@ public class MenuController {
     }
 
     @FXML
-    private void handleSound(ActionEvent event) {
+    public void initialize() {
+        volumeSprite = new Image(getClass().getResource("/images/menu/volume.png").toExternalForm());
+        btnVolume.setImage(volumeSprite);
+        btnVolume.setViewport(new Rectangle2D(0, 0, 70, 70));
+    }
 
+    @FXML
+    private void toggleVolume() {
+        mute = !mute;
+        if (mute) {
+            btnVolume.setViewport(new Rectangle2D(70, 0, 70, 70));
+            // xu li am thanh
+        } else {
+            btnVolume.setViewport(new Rectangle2D(0, 0, 70, 70));
+            // xu li am thanh
+        }
     }
 
     @FXML
     private void handleBack(ActionEvent event) {
-        Stage stage = (Stage) btnHelp.getScene().getWindow();
-        openGame(stage);
+        Stage stage = (Stage) btnBack.getScene().getWindow();
+        stage.close();
     }
 
 }
--- src/main/java/com/example/arkanoid/main/Main.java
+++ src/main/java/com/example/arkanoid/main/Main.java
@@ -3,10 +3,9 @@ package com.example.arkanoid.main;
 import com.example.arkanoid.controllers.GameController;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
 public class Main extends Application {
 
@@ -15,16 +14,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/arkanoid/views/MenuView.fxml"));
-        Parent root = loader.load();
-
-        Scene scene = new Scene(root, 960, 640);
-        primaryStage.setTitle("Arkanoid Menu");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-
-    }
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/arkanoid/views/MenuView.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, WIDTH, HEIGHT);
+            primaryStage.setTitle("Arkanoid Menu");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     public static void main(String[] args) {
         launch(args);
